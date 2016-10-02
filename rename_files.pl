a899ef7fdaa5b19551781590013fcecaa1cf43b0
Jonathan Lee <jon.lee.317@gmail.com>
2016-10-02 16:53:16 -0700

    docs: Add usage description to rename_files

--- rename_files.pl
+++ rename_files.pl
@@ -1,18 +1,30 @@
 #!/usr/local/bin/perl
 
-
-
+# Rename all the files in the directory specified by the user
+# 1) Update the directory information under my $diretory e.g.  C:\Projects
+# 2) Under $files_new, change the text "change_from" to the value you want to change from
+# 3) Under $files_new, change the the "change_to" to the value you want the files to be changed to
+#
+# e.g. If you have a list of files with the names:
+# super01.txt
+# super02.txt
+# super03.txt
+# super04.txt
+# and you want to remove the word super from all of the files
+# Set the following line to the following
+# $files_new =~ s/super//ig;
+# change_from = super
+# change_to = "" (essentially nothing)
+#
 #----------- open directory ---------------
 
-### set the correct director here:
-my $directory = 'Y:\Projects\X145\Design\sym';
-
+# Set the correct director here:
+my $directory = 'C:\Projects';
 opendir(DIR, $directory) or die "couldn't open $directory: $!\n";
 
 #-------- open files text --------------
 open (netlist_mod, ">files.txt") ||
 die "Error opening file!";
-
 
 #--- Store opened file in an array ----
 my @files= readdir DIR;
@@ -20,13 +32,11 @@ closedir DIR;
 
 #---------- Processing data  -------------
 
-
 foreach $files (@files)
 {
 	$files_new = $files;
 
-	### depending what project number you want to change, do it here.
-	$files_new =~ s/x122/x145/ig;
+	$files_new =~ s/change_from/change_to/ig;
 
 	rename($files, $files_new) or  
         warn "Couldn't rename $files to $files_new: $!\n";
